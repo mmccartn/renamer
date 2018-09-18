d2bfebc98d1bd04e6c3f9032cf1ac2877d50e210
Matt McCartney <mmccartn@usc.edu>
2018-09-17 20:26:34 -0700

    It writes

---
+++ README.md
@@ -0,0 +1,16 @@
+# Renamer
+Ad-hoc renaming tool for organizing my music collection.
+
+Usage
+-----
+```
+$ node main.js --help
+usage: main.js [-h] [-v] [-p PATH]
+
+Rename utility
+
+Optional arguments:
+  -h, --help            Show this help message and exit.
+  -v, --version         Show program's version number and exit.
+  -p PATH, --path PATH  Path to directory containing items to rename
+```
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
+
 const ArgumentParser = require('argparse').ArgumentParser
 const fs = require('fs')
 const path = require('path')
+const readlineSync = require('readline-sync')
 
 const dashSpace = function(name) {
     return name.replace(/([a-z])-([a-z])/i, '$1 - $2')
@@ -13,13 +15,24 @@ const flacParen = function(name) {
 
 const main = function(args) {
     const targetPath = args.absPath ? args.path : path.join(__dirname, args.path)
-    const opts = { withFileTypes : true }
+    const opts = { withFileTypes: true }
     const targets = fs.readdirSync(targetPath, opts)
         .filter(item => item.isDirectory())
-        .map(item => item.name)
-        .map(name => dashSpace(name))
-        .map(name => flacParen(name))
-    console.log(targets)
+        .map(item => Object.assign(item, { newName: item.name }))
+        .map(item => Object.assign(item, { newName: dashSpace(item.newName) }))
+        .map(item => Object.assign(item, { newName: flacParen(item.newName) }))
+    console.info(targets.map(item => item.newName))
+    let count = 0
+    if (readlineSync.keyInYN('Apply new names?')) {
+        targets.forEach(item => {
+            fs.renameSync(
+                path.join(targetPath, item.name),
+                path.join(targetPath, item.newName)
+            )
+            count++
+        })
+    }
+    console.info('Renamed', count)
 }
 
 if (require.main === module) {
